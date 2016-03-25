24ef79337372e35867905b5b0e9ab238f3bcc6ae
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-03-25 15:26:36 -0400

    Added set and add unit test

--- MyASM/commandTests.cpp
+++ MyASM/commandTests.cpp
@@ -23,9 +23,52 @@ bool testSetCommand()
 	return true;
 }
 
+bool testAddCommand()
+{
+	SETUP_FOR_EXECUTION
+
+	registers.push_back(10);
+	registers.push_back(5);
+
+	command* theCommand = new add(10, 1, 0);
+	theCommand->execute(registers, programCounter);
+
+	if(registers[10] != 15)
+	{
+		cout <<"Test: Add Test Failure" << endl;
+		return false;
+	}
+
+	return true;
+}
+
+bool testSetAndAdd()
+{
+	SETUP_FOR_EXECUTION
+
+	std::vector <command*> commands;
+	commands.push_back(new set(1,10));
+	commands.push_back(new set(0, 5));
+	commands.push_back(new add(2, 1, 0));
+
+	commands[0]->execute(registers, programCounter);
+	commands[1]->execute(registers, programCounter);
+	commands[2]->execute(registers, programCounter);
+
+	if(registers[2] != 15)
+	{
+		cout << "Test: Set and Add Test Failure" << endl;
+		return false;
+	}
+
+	return true;
+}
+
 bool runTests()
 {
 	if(!testSetCommand()) return false;
+	if(!testAddCommand()) return false;
+	if(!testSetAndAdd()) return false;
 
 	cout << "Test: All Tests Passed!" << endl;
 	return true;
